9123b3bb96e164ec3844d45f3f85269edc751129
4cruzeta <4cruzetam@gmail.com>
2019-11-21 10:42:56 -0300

    Resolvido erro com ParseExact

--- Review/Program.cs
+++ Review/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Review.Entities;
 
 namespace Review
@@ -11,7 +12,8 @@ namespace Review
             Comment c1 = new Comment("Good to know this!");
             Comment c2 = new Comment("Nice!");
             Post p1 = new Post(
-                    DateTime.Parse("06/21/2018 13:05:44"),
+                    DateTime.ParseExact("21/06/2018 13:05:44", "dd/MM/yyyy HH:mm:ss",   // Resolvido com ParseExact !
+                    CultureInfo.InvariantCulture),                                      
                     "TV Sound Exit",
                     "It has a headphone plug",
                     12);
@@ -21,9 +23,9 @@ namespace Review
             Comment c3 = new Comment("Nice!");
             Comment c4 = new Comment("Thanks for the information!");
             Post p2 = new Post(
-                    DateTime.Parse("07/28/2018 23:14:19"),
-                    "TV Voltage",
-                    "It is Bivolt 90v/240v",
+                    DateTime.Parse("07/28/2018 23:14:19"),          // No código original foi preciso usar mês/ano !
+                    "TV Voltage",                                   // Problema foi resolvido com ParseExact na    
+                    "It is Bivolt 90v/240v",                        // ocorrencia anterior. Será que é o OSX (MAC) ?
                     5);
             p2.AddComment(c3);
             p2.AddComment(c4);
